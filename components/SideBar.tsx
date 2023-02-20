903059c2af9ca7268252d6915ed9f91e3f535ebf
Jan Szczepan <janszczepan62@gmail.com>
2023-02-20 21:13:34 +0100

    User profile image in sidebar and signout functionality

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,11 @@
+'use client'
+
+import { signOut, useSession } from 'next-auth/react'
 import NewChat from './NewChat'
 
 function SideBar() {
+   const { data: session } = useSession()
+
    return (
       <div className='p-2 flex flex-col h-screen'>
          <div className='flex-1'>
@@ -8,6 +13,21 @@ function SideBar() {
                <NewChat />
             </div>
          </div>
+         {session && (
+            <button
+               type='button'
+               className='mx-auto my-2 cursor-pointer'
+               onClick={() => signOut()}
+            >
+               <img
+                  src={
+                     session.user?.image || '../public/images/UserProfile.png'
+                  }
+                  alt='User profile'
+                  className='w-12 h-12 rounded-full hover:opacity-50'
+               />
+            </button>
+         )}
       </div>
    )
 }
