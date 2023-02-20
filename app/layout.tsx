41d6e221f514110917cb3faeda61b04436551028
Jan Szczepan <janszczepan62@gmail.com>
2023-02-20 12:23:22 +0100

    SideBar and NewChat components JSX and styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react'
+import SideBar from '../components/SideBar'
 import './globals.css'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
@@ -7,6 +8,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
          <head />
          <body>
             <div className='flex'>
+               <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
+                  <SideBar />
+               </div>
                <div className='bg-[#343541] flex-1'>{children}</div>
             </div>
          </body>
